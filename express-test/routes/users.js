cca1519a3fb29de469a5cb9a98d6500ed93b6c73
FRANKIETANG <350558468@qq.com>
2017-12-13 00:11:44 +0800

    subscription

--- express-test/routes/users.js
+++ express-test/routes/users.js
@@ -20,11 +20,21 @@ router.post('/', (req, res) => {
   res.json(u);
 });
 
-router.get('/:userId', (req, res) => { // 前面加一个冒号就代表这个是作为一个参数处理
+router.get('/:userId/', (req, res) => { // 前面加一个冒号就代表这个是作为一个参数处理
   console.log(req.params.userId);
   const user = UserService.getUserById(Number(req.params.userId));
   res.locals.user = user;
   res.render('user');
+});
+
+// 增加一条订阅
+router.post('/:userId/subscription', (req, res, next) => { // 前面加一个冒号就代表这个是作为一个参数处理
+  try {
+    const sub = UserService.createSubscription(Number(req.params.userId), req.body.url);
+    res.json(sub);
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
--- express-test/services/user-service.js
+++ express-test/services/user-service.js
@@ -1,4 +1,5 @@
 const User = require('../models/in-demo/user');
+const Subscription = require('../models/in-demo/subscription');
 
 module.exports = {
   getAllUsers() {
@@ -9,5 +10,11 @@ module.exports = {
   },
   getUserById(userId) {
     return User.getOneById(userId);
+  },
+  createSubscription(UserId, url) {
+    const user = User.getOneById(UserId);
+    if (!user) throw Error('no such user!'); // 判断有没有用户没有就抛错
+    const sub = Subscription.insert(UserId, url); // 有就 insert 一个新的 userId 和 url
+    return sub;
   },
 };
